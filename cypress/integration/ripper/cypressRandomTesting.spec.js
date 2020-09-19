f63b46c3e157693577a6906892c9ef749a743074
ccordob <catalina.cordoba@morningstar.com>
2020-09-19 12:55:39 -0500

    update random testing

--- cypress/integration/ripper/cypressRandomTesting.spec.js
+++ cypress/integration/ripper/cypressRandomTesting.spec.js
@@ -2,12 +2,10 @@ describe('Web App under monkeys', function () {
 
     let url;
     let numberOfEvents;
-    let seeds;
 
     before(() => {
         url = Cypress.env('url');
         numberOfEvents = Cypress.env('events');
-        seeds = Cypress.env('seeds').split(';'); 
     })
 
     it('visits web app and survives monkeys', function () {
@@ -16,51 +14,62 @@ describe('Web App under monkeys', function () {
         randomEvent(numberOfEvents);
     });
 
+    function randomEvent(monkeysLeft) {
 
-
-function randomEvent(monkeysLeft) {
-
-    function getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min)) + min;
-    };
-    var randomeEventId = getRandomInt(0, seeds.length);
-    var monkeysLeft = monkeysLeft;
-    if (monkeysLeft > 0) {
-        var seedKeyValue = seeds[randomeEventId].split(':');
-        var seedObject = seedKeyValue[0];
-        var seedEvent = seedKeyValue[1];
-        cy.get(seedObject).then($elements => {
-            var randomElement = $elements.get(getRandomInt(0, $elements.length));
-            if (!Cypress.dom.isHidden(randomElement)) {
-                switch (seedEvent) {
-                    case 'click':
-                        cy.log('CLICK '+seedObject);
-                        cy.wrap(randomElement).click({ force: true });
-                        break;
-                    case 'type':
-                        cy.log('TYPE '+seedObject);
-                        cy.wrap(randomElement).type('Random Testing Text ' + randomeEventId, { force: true });
-                        break;
-                    case 'clear':
-                        cy.log('CLEAR '+seedObject);
-                        cy.wrap(randomElement).clear({ force: true });
-                        break;
-                    case 'dblclick':
-                        cy.log('DB CLICK '+seedObject);
-                        cy.wrap(randomElement).dblclick({ force: true });
-                        break;
-                    case 'scroll':
-                        cy.log('SCROLL '+seedObject);
-                        cy.wrap(randomElement).scrollIntoView({ force: true });
-                        break;
-                }
-                monkeysLeft = monkeysLeft - 1;
+        function getRandomInt(min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min)) + min;
+        };
+        var randomeEventId = getRandomInt(1, 5);
+        var monkeysLeft = monkeysLeft;
+        if (monkeysLeft > 0) {
+            switch (randomeEventId) {
+                case 1: // click on random link
+                    cy.get('a').then($links => {
+                        var randomLink = $links.get(getRandomInt(0, $links.length));
+                        if (!Cypress.dom.isHidden(randomLink)) {
+                            cy.wrap(randomLink).click({ force: true });
+                            monkeysLeft = monkeysLeft - 1;
+                        }
+                        cy.wait(500);
+                        randomEvent(monkeysLeft);
+                    });
+                    break;
+                case 2: // type in random field
+                    cy.get('input').then($inputs => {
+                        var randomInput = $inputs.get(getRandomInt(0, $inputs.length));
+                        if (!Cypress.dom.isHidden(randomInput)) {
+                            cy.wrap(randomInput).type("Moneky Test", { force: true });
+                            monkeysLeft = monkeysLeft - 1;
+                        }
+                        cy.wait(500);
+                        randomEvent(monkeysLeft);
+                    });
+                    break;
+                case 3: // click on random button
+                    cy.get('button').then($buttons => {
+                        var randomButton = $buttons.get(getRandomInt(0, $buttons.length));
+                        if (!Cypress.dom.isHidden(randomButton)) {
+                            cy.wrap(randomButton).click({ force: true });
+                            monkeysLeft = monkeysLeft - 1;
+                        }
+                        cy.wait(500);
+                        randomEvent(monkeysLeft);
+                    });
+                    break;
+                case 4: // click on random option
+                    cy.get('li').then($options => {
+                        var randomOption = $options.get(getRandomInt(0, $options.length));
+                        if (!Cypress.dom.isHidden(randomOption)) {
+                            cy.wrap(randomOption).click({ force: true });
+                            monkeysLeft = monkeysLeft - 1;
+                        }
+                        cy.wait(1000);
+                        randomEvent(monkeysLeft);
+                    });
+                    break;
             }
-            cy.wait(500);
-            randomEvent(monkeysLeft);
-        });
+        }
     }
-}
 });
